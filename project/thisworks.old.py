d13393e8301c939069f771b0020b57711b5d8d24
Johan Solbakken <johan.hvaler@gmail.com>
2024-10-05 16:29:21 +0200

    fix: warnings when importing keras

--- project/thisworks.old.py
+++ project/thisworks.old.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
-from typing import Optional
 from geopy.distance import geodesic
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
